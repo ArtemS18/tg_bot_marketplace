bc9bdaff9227de58ea70ef4a9e77b34dadec4fc5
ArtemS18 <65869064+ArtemS18@users.noreply.github.com>
2025-03-02 16:59:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bot import src
 if __name__ == '__main__':
-    TOKEN = "8029969009:AAE4MHCWWZv6tvY_EsJmpDoTNvM58D94CY8"
+    TOKEN = ""
     product_bot = src.ProductBot(TOKEN)
     product_bot.run()
